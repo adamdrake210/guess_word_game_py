82d80d325d65da5451e3e65e55089b53d5d10024
unknown <addrake@L-PRG-00821500.corp.ebay.com>
2016-08-17 13:33:02 +0200

    First commit

---
+++ guessLetters.py
@@ -0,0 +1,43 @@
+import random
+
+def get_random_word():
+    words = ["Pizza", "Cheese", "Apples"]
+    word = words[random.randint(0, len(words)-1)]
+    return word
+
+def show_word(word):
+    for character in word:
+        print(character, " ", end="")
+    print("")
+
+def get_guess():
+    print("Enter a letter: ")
+    return input()
+
+def play_word_game():
+    strikes = 0
+    max_strikes = 3
+    playing = True
+    
+    word = get_random_word()
+    blanked_word = "_" * len(word)
+
+    while playing:
+        show_word(blanked_word)
+        letter = get_guess()
+        
+        strikes +=1
+
+        if strikes >= max_strikes:
+            playing = False
+
+
+    if strikes >= max_strikes:
+        print("Loser!")
+    else:
+        print("Winner!")
+
+
+print("Game Started! Good Luck!")
+play_word_game()
+print("Game Over!")
